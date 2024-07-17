676bda954525d81cf2799a7f28eee94ef366bcca
pasindu8 <75705950+pasindu8@users.noreply.github.com>
2024-07-17 17:45:47 +0530

    PD BOT
    PD BOT

---
+++ config.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+
+// Other
+global.link = 'https://chat.whatsapp.com/CwVny6CagusDpw6fIII5UD'
+global.linkGroup = 'https://chat.whatsapp.com/CwVny6CagusDpw6fIII5UD'
+
+global.apikey = 'bf2d2cf29b3edc604b447983';
+global.ownername = "𝔡𝔲𝔩𝔰𝔥𝔞𝔫 ";
+global.owner = ['91723051652'];
+global.premium = ['91723051652'];
+global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
+global.author = "🐉𝕻𝕯 𝕭𝕺𝕿🐉";
+global.sessionName = 'session';
+global.prefa = [".", "!", "/", "?"];
+global.typemenu = "v1"
+global.menuType = "2"
+global.autoswview = true
+global.autoread = true
+global.available = false
+global.autoTyping = true
+global.autoRecord = true
+global.autoreact = false
+global.onlygroup = false
+global.onlypc = false
+global.welcome = true
+global.autoBlock = true
+ 
+/*REPLY MESSAGE*/
+global.mess = {
+    banned: '`You are *Banned* fron using commands!`',
+    bangc: '`This Group is *Banned* from using Commands!`',
+    success: '`✓ Success`',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
+    wait: '*Processing Your request*',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
+};
+
+global.limitawal = {
+    premium: 'Infinity',
+    free: 10
+};
